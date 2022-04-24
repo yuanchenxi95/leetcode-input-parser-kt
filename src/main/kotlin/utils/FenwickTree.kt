2b6129b0786bcd2b3d28c6fc116decddfbd5342f
Chenxi Yuan <yuanchenxi95@gmail.com>
2022-04-23 23:34:00 -0700

    Updates Fenwick Tree to use map

--- src/main/kotlin/utils/FenwickTree.kt
+++ src/main/kotlin/utils/FenwickTree.kt
@@ -1,13 +1,13 @@
 package utils
 
 class FenwickTree(val n: Int) {
-    val nums = LongArray(n)
+    val nums = mutableMapOf<Int, Long>()
 
     fun sum(rr: Int): Long {
         var r = rr
         var res = 0L
         while (r >= 0) {
-            res += nums[r]
+            res += nums.getOrDefault(r, 0)
             r = (r and (r + 1)) - 1
         }
         return res
@@ -16,7 +16,7 @@ class FenwickTree(val n: Int) {
     fun add(idx: Int, delta: Long) {
         var ii = idx
         while (ii < n) {
-            nums[ii] += delta
+            nums[ii] = nums.getOrDefault(ii, 0) + delta
             ii = ii or (ii + 1)
         }
     }
