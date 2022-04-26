ee73034af6732abc64b873cfaf6aa620ca52dc9f
Chenxi Yuan <yuanchenxi95@gmail.com>
2022-04-25 18:36:54 -0700

    Create UnionSet.kt

---
+++ src/main/kotlin/utils/UnionSet.kt
@@ -0,0 +1,26 @@
+package utils
+
+class UnionSet {
+  val unionSet = mutableMapOf<Int, Int>()
+  val length = mutableMapOf<Int, Int>()
+
+  fun find(ii: Int): Int {
+    val target = unionSet[ii] ?: return ii
+    val jj = find(target)
+    unionSet[ii] = jj
+    return jj
+  }
+
+  fun union(ii: Int, jj: Int): Int {
+    val left = find(ii)
+    val right = find(jj)
+    val newLength = getLength(right) + getLength(left)
+    length[right] = newLength
+    unionSet[left] = right
+    return newLength
+  }
+
+  fun getLength(ii: Int): Int {
+    return length.getOrDefault(ii, 1)
+  }
+}
